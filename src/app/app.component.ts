cd09d175690b722a4e425fb3216521318a7fbf2f
Aleksa <97457431+AleksaRoad@users.noreply.github.com>
2023-08-03 17:35:18 +0300

    feat(config): add eslint.json, eslintignore and scripts eslint(RSS-ECOMM-1_10, RSS-ECOMM-1_14)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'ec-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'e-commerce';
+  title = 'e-commerce'
 }
