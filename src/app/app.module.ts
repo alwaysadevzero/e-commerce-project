7d7dd88fba55927ba8ccf4dc591ee4d0dc315f8a
alwaysadevzero <119855949+alwaysadevzero@users.noreply.github.com>
2023-08-05 16:04:57 +0400

    feat(packaging): add environment variables logic in project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { EffectsModule } from '@ngrx/effects'
 import { StoreModule } from '@ngrx/store'
 import { StoreDevtoolsModule } from '@ngrx/store-devtools'
+import { NgxEnvModule } from '@ngx-env/core'
 import { TUI_SANITIZER, TuiAlertModule, TuiDialogModule, TuiRootModule } from '@taiga-ui/core'
 import { NgDompurifySanitizer } from '@tinkoff/ng-dompurify'
 
+import { environment } from '../environments/environment'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 
@@ -22,6 +24,7 @@ import { AppComponent } from './app.component'
     TuiDialogModule,
     TuiAlertModule,
     StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
+    NgxEnvModule,
   ],
   providers: [{ provide: TUI_SANITIZER, useClass: NgDompurifySanitizer }],
   bootstrap: [AppComponent],
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  api: import.meta.env['EC_APP_ENV'],
+}
