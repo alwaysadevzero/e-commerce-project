16b1e8ed0ebb7394936290cdd25f7b96a0369ae4
Anthony <119855949+alwaysadevzero@users.noreply.github.com>
2023-08-17 02:54:48 +0000

    feat(auth): add basic signup sdk implementation

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,10 +1,11 @@
 import { inject, Injectable } from '@angular/core'
-import { type Project } from '@commercetools/platform-sdk'
+import { type CustomerSignInResult, type MyCustomerDraft, type Project } from '@commercetools/platform-sdk'
 import { type Observable } from 'rxjs'
 import { fromPromise } from 'rxjs/internal/observable/innerFrom'
 import { map } from 'rxjs/operators'
 
 import { ApiClientBuilderService } from '../../core/services/api-client-builder.service'
+import { environment } from 'src/environments/environment'
 
 @Injectable({
   providedIn: 'root',
@@ -15,6 +16,18 @@ export class AuthHttpService {
   public login({ username, password }: { username: string; password: string }): Observable<Project> {
     return fromPromise(
       this.apiClientBuilderService.createApiClientWithPasswordFlow(username, password).get().execute(),
+    ).pipe(map(({ body }) => body))
+  }
+
+  public signup(customer: MyCustomerDraft): Observable<CustomerSignInResult> {
+    return fromPromise(
+      this.apiClientBuilderService.getApi
+        .me()
+        .signup()
+        .post({
+          body: customer,
+        })
+        .execute(),
     ).pipe(map(({ body }) => body))
   }
 }
